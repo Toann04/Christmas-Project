ac78783a3a90957fc82f5c8c3e61a78946061b12
HoangBuiVietDuc <86623355+HoangBuiVietDuc@users.noreply.github.com>
2022-12-18 09:48:06 +0700

    Update onClick.js

--- Christmas Web/JS/onClick.js
+++ Christmas Web/JS/onClick.js
@@ -3,8 +3,8 @@
 function buynow1() {
     var product1 = document.getElementById('product-1').parentElement
     var productImg1 = product1.querySelector('img').src
-    var productName1 = product1.querySelector('#name').innerText
-    var productPrice1 = product1.querySelector('#price').innerText
+    var productName1 = product1.querySelector('#name-1').innerText
+    var productPrice1 = product1.querySelector('#price-1').innerText
     // console.log(product);
     console.log(productImg1);
     console.log(productName1);
@@ -14,11 +14,11 @@ function buynow1() {
 function buynow2() {
     var product2 = document.getElementById('product-2').parentElement
     var productImg2 = product2.querySelector('img').src
-    var productName2 = product2.querySelector('#name').innerText
-    var productPrice2 = product2.querySelector('#price').innerText
+    var productName2 = product2.querySelector('#name-2').innerText
+    var productPrice2 = product2.querySelector('#price-2').innerText
     // console.log(product);
-    console.log(productImg);
-    console.log(productName);
-    console.log(productPrice);
+    console.log(productImg2);
+    console.log(productName2);
+    console.log(productPrice2);
 }
 
