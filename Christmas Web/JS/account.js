391883599b4d4f952423cb4b3550e99f84c4ceb2
HoangBuiVietDuc <86623355+HoangBuiVietDuc@users.noreply.github.com>
2022-12-18 09:36:37 +0700

    Add files via upload

---
+++ Christmas Web/JS/account.js
@@ -0,0 +1,52 @@
+
+
+let user = [
+    {
+        username: "Duc",
+        password: 28102006,
+    },
+    {
+        username: "Harry",
+        password: 123456789,
+    },
+    {
+        username: "abc",
+        password: 123,
+    },
+]
+
+// function getInfo() {
+//     var username = document.getElementById("username").value
+//     var password = document.getElementById("password").value
+//     for(i = 0; i < user .length; i++) {
+//         if(username == user[i].username && password == user[i].password) {
+//             console.log(`${username} is successful logged in!`);
+//             return
+//         }
+//     }
+//     console.log("Incorrect username or password, try again");
+// }
+
+// ///////////////////////////////////////////////////////////////////////
+
+
+
+function getInfo() {
+    let username = localStorage.setItem.getElementById("username").value
+    let password = localStorage.setItem.getElementById("password").value
+    // for(i = 0; i < user .length; i++) {
+    //     if(username == user[i].username && password == user[i].password) {
+    //         console.log(`${username} is successful logged in!`);
+    //         return
+    //     }
+    // }
+    // console.log("Incorrect username or password, try again");
+}
+
+
+
+// for() {
+//     if(console.log("incorrect username or password")) {
+//         console.log("welcome");
+//     }
+// }
---
+++ Christmas Web/JS/onClick.js
@@ -0,0 +1,24 @@
+// const btn_buynow = document.querySelectorAll("button")
+// console.log(btn_buynow);
+function buynow1() {
+    var product1 = document.getElementById('product-1').parentElement
+    var productImg1 = product1.querySelector('img').src
+    var productName1 = product1.querySelector('#name').innerText
+    var productPrice1 = product1.querySelector('#price').innerText
+    // console.log(product);
+    console.log(productImg1);
+    console.log(productName1);
+    console.log(productPrice1);
+}
+
+function buynow2() {
+    var product2 = document.getElementById('product-2').parentElement
+    var productImg2 = product2.querySelector('img').src
+    var productName2 = product2.querySelector('#name').innerText
+    var productPrice2 = product2.querySelector('#price').innerText
+    // console.log(product);
+    console.log(productImg);
+    console.log(productName);
+    console.log(productPrice);
+}
+
