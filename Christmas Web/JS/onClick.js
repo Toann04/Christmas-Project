96b731ed52fb6cd8e2a2498708f3f3c7157e9bda
HoangBuiVietDuc <86623355+HoangBuiVietDuc@users.noreply.github.com>
2023-01-07 23:31:28 +0700

    Update onClick.js

--- Christmas Web/JS/onClick.js
+++ Christmas Web/JS/onClick.js
@@ -1,5 +1,3 @@
-// const btn_buynow = document.querySelectorAll("button")
-// console.log(btn_buynow);
 function buynow1() {
     var product1 = document.getElementById('product-1').parentElement
     var productImg1 = product1.querySelector('img').src
@@ -21,4 +19,114 @@ function buynow2() {
     console.log(productName2);
     console.log(productPrice2);
 }
+function buynow3() {
+    var product3 = document.getElementById('product-3').parentElement
+    var productImg3 = product3.querySelector('img').src
+    var productName3 = product3.querySelector('#name-3').innerText
+    var productPrice3 = product3.querySelector('#price-3').innerText
+    // console.log(product);
+    console.log(productImg3);
+    console.log(productName3);
+    console.log(productPrice3);
+}
 
+// /////////////////////////////////////////////////////////////////////////
+
+function treebuynow1() {
+    var product1 = document.getElementById('tree-1').parentElement
+    var productImg1 = product1.querySelector('img').src
+    var productName1 = product1.querySelector('#name-1').innerText
+    var productPrice1 = product1.querySelector('#price-1').innerText
+    // console.log(product);
+    console.log(productImg1);
+    console.log(productName1);
+    console.log(productPrice1);
+}
+
+function treebuynow2() {
+    var product2 = document.getElementById('tree-2').parentElement
+    var productImg2 = product2.querySelector('img').src
+    var productName2 = product2.querySelector('#name-2').innerText
+    var productPrice2 = product2.querySelector('#price-2').innerText
+    // console.log(product);
+    console.log(productImg2);
+    console.log(productName2);
+    console.log(productPrice2);
+}
+function treebuynow3() {
+    var product3 = document.getElementById('tree-3').parentElement
+    var productImg3 = product3.querySelector('img').src
+    var productName3 = product3.querySelector('#name-3').innerText
+    var productPrice3 = product3.querySelector('#price-3').innerText
+    // console.log(product);
+    console.log(productImg3);
+    console.log(productName3);
+    console.log(productPrice3);
+}
+
+// /////////////////////////////////////////////////////////////////////////
+
+function garlandbuynow1() {
+    var product1 = document.getElementById('garland-1').parentElement
+    var productImg1 = product1.querySelector('img').src
+    var productName1 = product1.querySelector('#name-1').innerText
+    var productPrice1 = product1.querySelector('#price-1').innerText
+    // console.log(product);
+    console.log(productImg1);
+    console.log(productName1);
+    console.log(productPrice1);
+}
+function garlandbuynow2() {
+    var product2 = document.getElementById('garland-2').parentElement
+    var productImg2 = product2.querySelector('img').src
+    var productName2 = product2.querySelector('#name-2').innerText
+    var productPrice2 = product2.querySelector('#price-2').innerText
+    // console.log(product);
+    console.log(productImg2);
+    console.log(productName2);
+    console.log(productPrice2);
+}
+function garlandbuynow3() {
+    var product3 = document.getElementById('garland-3').parentElement
+    var productImg3 = product3.querySelector('img').src
+    var productName3 = product3.querySelector('#name-3').innerText
+    var productPrice3 = product3.querySelector('#price-3').innerText
+    // console.log(product);
+    console.log(productImg3);
+    console.log(productName3);
+    console.log(productPrice3);
+}
+
+// /////////////////////////////////////////////////////////////////////////
+
+
+function sockbuynow1() {
+    var product1 = document.getElementById('sock-1').parentElement
+    var productImg1 = product1.querySelector('img').src
+    var productName1 = product1.querySelector('#name-1').innerText
+    var productPrice1 = product1.querySelector('#price-1').innerText
+    // console.log(product);
+    console.log(productImg1);
+    console.log(productName1);
+    console.log(productPrice1);
+}
+function sockbuynow2() {
+    var product2 = document.getElementById('sock-2').parentElement
+    var productImg2 = product2.querySelector('img').src
+    var productName2 = product2.querySelector('#name-2').innerText
+    var productPrice2 = product2.querySelector('#price-2').innerText
+    // console.log(product);
+    console.log(productImg2);
+    console.log(productName2);
+    console.log(productPrice2);
+}
+function sockbuynow3() {
+    var product3 = document.getElementById('sock-3').parentElement
+    var productImg3 = product3.querySelector('img').src
+    var productName3 = product3.querySelector('#name-3').innerText
+    var productPrice3 = product3.querySelector('#price-3').innerText
+    // console.log(product);
+    console.log(productImg3);
+    console.log(productName3);
+    console.log(productPrice3);
+}
